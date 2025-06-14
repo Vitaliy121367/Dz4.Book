fde8d5228547d9c77c7ca0c650fc76a038058ba5
Vitaliy <qw929496@gmail.com>
2025-06-14 22:32:35 +0300

    _timer

--- Dz4/Program.cs
+++ Dz4/Program.cs
@@ -12,33 +12,6 @@ namespace Dz4
         {
             Timer _timer;
 
-            //var newUser = await libraryService.CreateUserAsync(new User(1,"Alice"));
-            //Console.WriteLine(newUser);
-
-            //var newBook = await libraryService.CreateBookAsync(new Book(6, "1984", "George Orwell", true, null));
-            //Console.WriteLine(newBook);
-
-            //var updatedUser = await libraryService.UpdateUserAsync(1, new User(1, "Alice Updated"));
-            //Console.WriteLine(updatedUser);
-
-            //var updatedBook = await libraryService.UpdateBookAsync(6, new Book(6, "1984", "G. Orwell", true, null));
-            //Console.WriteLine(updatedBook);
-
-            //var history1 = await libraryService.takeReturnBook(1, 6);
-            //Console.WriteLine(history1);
-
-            //var history2 = await libraryService.takeReturnBook(1, 6);
-            //Console.WriteLine(history2);
-
-            //var deletedHistory = await libraryService.delBookHistoryAsync(1);
-            //Console.WriteLine(deletedHistory);
-
-            //var deletedBook = await libraryService.delBookAsync(6);
-            //Console.WriteLine(deletedBook);
-
-            //var deletedUser = await libraryService.delUserAsync(1);
-            //Console.WriteLine(deletedUser);
-
             _timer = new Timer(ShowUsersAndBooks, null, 0, 1000);
             Console.ReadKey();
         }
